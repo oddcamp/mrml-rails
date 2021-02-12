6b4f59449cb43d96d485e3dee401f7a6efb7ca67
Joakim Nylén <git@joakim.nylen.nu>
2021-02-12 07:48:45 +0100

    Fix

--- lib/mrml/rails/railtie.rb
+++ lib/mrml/rails/railtie.rb
@@ -3,11 +3,11 @@ require 'rails'
 module Mrml
   module Rails
     class Railtie < ::Rails::Railtie
-      config.mjml = Mrml
-      config.app_generators.mailer :template_engine => :mjml
+      #config.mjml = Mrml
+      #config.app_generators.mailer :template_engine => :mjml
 
       initializer "mrml-rails.register_template_handler" do
-        ActionView::Template.register_template_handler :mjml, Mrml::Handler.new
+        ActionView::Template.register_template_handler :mjml, Mrml::Rails::Handler.new
         Mime::Type.register_alias "text/html", :mjml
       end
     end
